303731e629bb3e053888646992146cdb09ceeff4
joseph chan <justinitsuj0428@gmail.com>
2019-06-05 02:47:41 +0800

    更新（財報沒資料處理）

--- crawler_foreigncapital_cashflow .py
+++ crawler_foreigncapital_cashflow .py
@@ -1,9 +1,116 @@
 from selenium import webdriver
 import time
-com_num = input('股票代碼：')
-print('===')
-driver = webdriver.Chrome('/Users/chanpohung/Desktop/chromedriver')
 
+#Good Info (2012~2018)
+co_id = input("輸入四位數股票代號: ")  # 輸入四位數股票代號
+
+from selenium.webdriver.common.keys import Keys #模擬鍵盤操作
+browser = webdriver.Chrome('/Users/chanpohung/Desktop/chromedriver')
+url = "https://goodinfo.tw/StockInfo/StockBzPerformance.asp?STOCK_ID="
+url += co_id
+browser.get(url)
+
+time.sleep(4)
+
+#找指標
+index = dict() #以指標為key的dict, 裡面放不同年份的資料
+year = ["2012","2013","2014","2015","2016","2017","2018"]
+
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(browser.page_source, 'html.parser')
+a_tags = soup.select('div#divFinDetail table.solid_1_padding_4_0_tbl tbody tr td')  #標籤名稱.class / 標籤名稱#id屬性
+
+#毛利率
+index['毛利率'] = []
+for i in range(0, len(a_tags)):
+    if a_tags[i].text in year:
+        try:
+            tmp = float(a_tags[i+12].text)
+        except Exception as inst:
+            tmp = a_tags[i+12].text
+        index['毛利率'].append(tmp)
+
+#淨利率
+index['淨利率'] = []
+for i in range(0, len(a_tags)):
+    if a_tags[i].text in year:
+        try:
+            tmp = float(a_tags[i+15].text)
+        except Exception as inst:
+            tmp = a_tags[i+15].text
+        index['淨利率'].append(tmp)
+
+#殖利率
+index['殖利率'] = []
+from selenium.webdriver.common.keys import Keys #模擬鍵盤操作
+browser.find_element_by_xpath("//input[@value='股利統計'][@type='button']").click()
+time.sleep(2)
+soup = BeautifulSoup(browser.page_source, 'html.parser')
+b_tags = soup.select('div#divFinDetail table.solid_1_padding_4_0_tbl tbody tr td')  #標籤名稱.class / 標籤名稱#id屬性
+
+for i in range(0, len(b_tags)):
+    if b_tags[i].text in year:
+        try:
+            tmp = float(b_tags[i+15].text)
+        except Exception as inst:
+            tmp = b_tags[i+15].text
+        index['殖利率'].append(tmp)
+
+#盈餘分配率
+index['盈餘分配率'] = []
+for i in range(0, len(b_tags)):
+    if b_tags[i].text in year:
+        try:
+            tmp = float(b_tags[i+18].text)
+        except Exception as inst:
+            tmp = b_tags[i+18].text
+        index['盈餘分配率'].append(tmp)
+
+#淨值比(PBR)
+browser.find_element_by_xpath("//input[@value='PER/PBR'][@type='button']").click()
+time.sleep(2)
+soup = BeautifulSoup(browser.page_source, 'html.parser')
+c_tags = soup.select('div#divFinDetail table.solid_1_padding_4_0_tbl tbody tr td')  #標籤名稱.class / 標籤名稱#id屬性
+index['淨值比(PBR)'] = []
+for i in range(0, len(c_tags)):
+    if c_tags[i].text in year:
+        try:
+            tmp = float(c_tags[i+16].text)
+        except Exception as inst:
+            tmp = c_tags[i+16].text
+        index['淨值比(PBR)'].append(tmp)
+
+#本益比(PER)
+index['本益比(PER)'] = []
+for i in range(0, len(c_tags)):
+    if c_tags[i].text in year:
+        try:
+            tmp = float(c_tags[i+12].text)
+        except Exception as inst:
+            tmp = c_tags[i+12].text
+        index['本益比(PER)'].append(tmp)
+
+#整理排序
+p2_up_rank = []
+for key in index:
+    while '-' in index[key]:
+        index[key].remove('-')
+    if index[key] != []:
+        p2_up_targetstock = index[key][0]  # 得到最新的資料以便之後排名用
+        index[key].sort()  # 把獲取的資料排序
+        if key == '淨值比(PBR)' or key == '本益比(PER)':
+            pass
+        else:
+            index[key].reverse()
+        p2_up_rank.append(str(index[key].index(p2_up_targetstock) + 1) + '/' + str(len(index[key])))
+    else:
+        print('無資料：' + str(key))
+print('===第二部分上半部照順序的排名===')
+print(p2_up_rank)
+print('======')
+
+#driver = webdriver.Chrome('/Users/chanpohung/Documents/chromedriver')
 
 class Foreigncapital:  # 外資持股比率的Class
 
@@ -14,19 +121,21 @@ class Foreigncapital:  # 外資持股比率的Class
 
     def get_fcdriver(self):  # 得到該時期該股號的資料頁面
 
-        self.get_fcdriver = driver.get("https://goodinfo.tw/StockInfo/ShowK_Chart.asp?STOCK_ID="
+        self.get_fcdriver = browser.get("https://goodinfo.tw/StockInfo/ShowK_Chart.asp?STOCK_ID="
                                  + str(self.com_num) + "&CHT_CAT2=" + str(self.period) + "")
         self.history_num = []
+        self.p3_rank = str()
+
 
     def get_fc(self):  # 抓到對應的值
 
         for j in range(119):
             try:
                 if self.period == 'MONTH' or 'WEEK':
-                    table_text = driver.find_element_by_xpath('//*[@id="row' + str(j) + '"]/td[17]/nobr').text
+                    table_text = browser.find_element_by_xpath('//*[@id="row' + str(j) + '"]/td[17]/nobr').text
 
                 if self.period == 'DATE':
-                    table_text = driver.find_element_by_xpath('//*[@id="row' + str(j) + '"]/td[16]/nobr').text
+                    table_text = browser.find_element_by_xpath('//*[@id="row' + str(j) + '"]/td[16]/nobr').text
 
                 if table_text != '':
                     self.history_num.append(float(table_text))
@@ -39,39 +148,44 @@ class Foreigncapital:  # 外資持股比率的Class
 
     def print_fc(self):  # 抓到最新資料在list裡面的排名然後印出
 
-        print('現在外資持股比率：' + str(self.latest_num) + ' %')
-
         if self.period == 'DATE':
-            print('短期外資持股排名：' + str(self.history_num.index(self.latest_num) + 1) + '/'
-                  + str(len(self.history_num)))
+            p3d_rank = (str(self.history_num.index(self.latest_num) + 1) + '/'+ str(len(self.history_num)))
+            self.p3_rank += (p3d_rank)
 
         elif self.period == 'WEEK':
-            print('中期外資持股排名：' + str(self.history_num.index(self.latest_num) + 1) + '/'
-                  + str(len(self.history_num)))
+            p3w_rank = (str(self.history_num.index(self.latest_num) + 1) + '/' + str(len(self.history_num)))
+            self.p3_rank += (p3w_rank)
 
         elif self.period == 'MONTH':
-            print('長期外資持股排名：' + str(self.history_num.index(self.latest_num) + 1) + '/'
-                  + str(len(self.history_num)))
+            p3m_rank = (str(self.history_num.index(self.latest_num) + 1) + '/' + str(len(self.history_num)))
+            self.p3_rank += (p3m_rank)
 
 
 def fc_fuc_all():  # 定義一個函數一次執行三個period
 
-    sf = Foreigncapital(com_num,'DATE')
+    p3_rank = []
+
+    sf = Foreigncapital(co_id,'DATE')
     sf.get_fcdriver()
     sf.get_fc()
     sf.print_fc()
-    print('===')
+    p3_rank.append(sf.p3_rank)
 
-    mf = Foreigncapital(com_num,'WEEK')
+    mf = Foreigncapital(co_id,'WEEK')
     mf.get_fcdriver()
     mf.get_fc()
     mf.print_fc()
-    print('===')
+    p3_rank.append(mf.p3_rank)
 
-    lf = Foreigncapital(com_num,'MONTH')
+    lf = Foreigncapital(co_id,'MONTH')
     lf.get_fcdriver()
     lf.get_fc()
     lf.print_fc()
+    p3_rank.append(lf.p3_rank)
+
+    print('===第三部分短中長排名===')
+    print(p3_rank)
+    print('======')
 
 
 class Cashflow:  # 抓到現金流量的class
@@ -82,17 +196,26 @@ class Cashflow:  # 抓到現金流量的class
 
     def get_cashflow(self):  # 抓到我要的值
 
-        self.get_cashflow_web = driver.get('https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID=' + str(self.com_num))
+        self.get_cashflow_web = browser.get('https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID=' + str(self.com_num))
 
         time.sleep(3)
 
-        self.business_cashflow = driver.find_element_by_xpath('//*[@id="FINANCE_CF_M"]/tbody/tr[5]/td[2]').text
-        self.investment_cashflow = driver.find_element_by_xpath('//*[@id="FINANCE_CF_M"]/tbody/tr[5]/td[3]').text
-        self.credit_cashflow = driver.find_element_by_xpath('//*[@id="FINANCE_CF_M"]/tbody/tr[5]/td[4]').text
+        self.revenue = browser.find_element_by_xpath('//*[@id="FINANCE_INCOME_M"]/tbody/tr[5]/td[2]').text
+        self.industry = browser.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[3]/table/tbody/tr[2]/td[3]'
+                                                     '/table[1]/tbody/tr[3]/td[2]').text
+        self.capitalamount = browser.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[3]/table/tbody/tr[2]'
+                                                          '/td[3]/table[1]/tbody/tr[5]/td[2]/nobr').text
+        self.business_cashflow = browser.find_element_by_xpath('//*[@id="FINANCE_CF_M"]/tbody/tr[5]/td[2]').text
+        self.investment_cashflow = browser.find_element_by_xpath('//*[@id="FINANCE_CF_M"]/tbody/tr[5]/td[3]').text
+        self.credit_cashflow = browser.find_element_by_xpath('//*[@id="FINANCE_CF_M"]/tbody/tr[5]/td[4]').text
 
 
     def print_cashflow(self):  # 把資料如果超過千億會有的逗號移掉並印出對應意義
 
+        print('===')
+        print('產業別：' + self.industry)
+        print('資本額：' + self.capitalamount)
+        print('營業額：' + self.revenue + '億')
         print('===')
 
         if len(self.business_cashflow) > 4:
@@ -144,12 +267,11 @@ class Cashflow:  # 抓到現金流量的class
 
         if business < 0 and investment < 0 and credit < 0:
 
-            print('公司手頭現金很多')
+            print('公司手頭現金可能很多')
 
   # 執行上面的class
 fc_fuc_all()
-c = Cashflow(com_num)
+c = Cashflow(co_id)
 c.get_cashflow()
 c.print_cashflow()
-  # 下面還有要排蟲的話就不要執行，不然會error
-driver.close()
+browser.close()
